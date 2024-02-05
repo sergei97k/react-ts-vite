a8bd07cabf2ad8e2db9037b647b23332723e7bc0
Serhii Kononov <sergei97k@gmail.com>
2024-02-05 20:56:25 +0200

    added example with ssml text

--- src/components/TextToSpeech.tsx
+++ src/components/TextToSpeech.tsx
@@ -1,31 +1,35 @@
 import { memo, useCallback, useEffect, useState } from "react";
 import { useSpeech } from "../hooks/useSpeech";
 
+// ssml
+const text =
+  '<speak>Step 1, take a deep breath. <break time="1s"/>Step 2, exhale.</speak>';
+
 export const TextToSpeech = memo(() => {
-  const [text, setText] = useState("");
+  // const [text, setText] = useState("");
   const { onPlay, onStop } = useSpeech({ text });
 
-  const fetchTitle = useCallback(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => response.json())
-      .then(({ title }) => {
-        setText(title);
-      });
-  }, []);
+  // const fetchTitle = useCallback(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts/1")
+  //     .then((response) => response.json())
+  //     .then(({ title }) => {
+  //       setText(title);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    fetchTitle();
-  }, [fetchTitle]);
+  // useEffect(() => {
+  //   fetchTitle();
+  // }, [fetchTitle]);
 
-  const handleLoadMore = useCallback(() => {
-    fetchTitle();
-  }, [fetchTitle]);
+  // const handleLoadMore = useCallback(() => {
+  //   fetchTitle();
+  // }, [fetchTitle]);
 
   return (
     <div>
       <button onClick={onPlay}>Play</button>
       <button onClick={onStop}>Pause</button>
-      <button onClick={handleLoadMore}>Load more</button>
+      {/* <button onClick={handleLoadMore}>Load more</button> */}
     </div>
   );
 });
